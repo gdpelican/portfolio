dd3e5a0b24cf6780adedde413699de7990158e50
James Kiesel <james.kiesel@gmail.com>
2014-04-21 10:22:55 -0700

    Update gruntfile to concat scss files and minify js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,50 +2,55 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    folders: {
+    	src: 	'src',
+    	dist: 	'dist',
+    	tmp: 	'<%= folders.src %>/tmp',
+    	scss: 	'<%= folders.src %>/css',
+    	coffee: '<%= folders.src %>/js',
+    	hbs: 	'<%= folders.src %>/templates'
+    },
     filesets: {
-    	scss: 	'src/css/**/*.scss',
-    	css: 	'src/tmp/*.css',
-    	coffee: 'src/js/**/*.coffee',
-    	js:		'src/tmp/*.js',
-    	hbs:	'src/templates/**/*.hbs'
+    	scss: 	'<%= folders.scss %>/**/*.scss',
+    	css: 	'<%= folders.tmp %>/*.css',
+    	coffee: '<%= folders.coffee %>/**/*.coffee',
+    	js:		'<%= folders.tmp %>/*.js',
+    	hbs:	'<%= folders.hbs %>/**/*.hbs'
     },
 	coffee: {
 	  options: { join: true },
       compile: {
-        files: { 'src/tmp/app.js': '<%= filesets.coffee %>' }
+        files: { '<%= folders.tmp %>/app.js': '<%= filesets.coffee %>' }
       }
     },
     sass: {
       compile: {
       	options: { style: 'compressed' },
-        files: [
-          { 'src/tmp/style.css': 'src/css/style.scss' },
-          { 'src/tmp/normalize.css': 'src/css/normalize.scss'} 
-        ] 	
+        files: { '<%= folders.dist %>/style.css': '<%= folders.tmp %>/style.scss' }	
       }
     },
     emberTemplates: {
       compile: {
       	options: { templateBasePath: 'src/templates' },
-      	files: { 'src/tmp/templates.js': '<%= filesets.hbs %>' }
+      	files: { '<%= folders.tmp %>/templates.js': '<%= filesets.hbs %>' }
       }
     },
     concat: {
       options: { separator: ';' },
-      css: {
-      	src: '<%= filesets.css %>',
-      	dest: 'dist/style.css'
+      scss: {
+      	src: '<%= filesets.scss %>',
+      	dest: '<%= folders.tmp %>/style.scss'
       }
     },
     uglify: {
     	js: {
-    		files: { 'dist/app.js': ['<%= filesets.js %>'] }
+    		files: { '<%= folders.dist %>/app.js': ['<%= filesets.js %>'] }
     	}
     },
     watch: {
       scss: {
       	files: ['<%= filesets.scss %>'],
-      	tasks: ['sass', 'concat'],
+      	tasks: ['concat', 'sass'],
       	options: { spawn: false, reload: true }
       },
 	  coffee: {
